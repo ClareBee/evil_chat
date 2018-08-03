2455fc608c6a8ad7022977e1d593f7b85541bc53
Clare Blackburne <clare.blackburne@castlightfinancial.com>
2018-08-03 11:34:57 +0100

    Tells controllers to access erb partials from Frontend instead of Views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # protects against Cross-Site Request Forgery CSRF
+  protect_from_forgery with: :exception
+  # tells controllers to find ERB partials in frontend folder
+  prepend_view_path Rails.root.join("frontend")
 end
