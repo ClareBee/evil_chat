56a585e1a95bfb6a1ce18d2e4a153f01abcf3874
Clare <chblackburne@gmail.com>
2018-08-11 15:02:32 +0100

    Function to insert message into dom

--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,1 +1,25 @@
+import { setCallback } from "client/chat";
+import "components/message/message";
 import "./messages.css";
+
+function scrollToBottom(element) {
+  // eslint-disable-next-line
+  element.scrollTop = element.scrollHeight;
+}
+
+const messages = document.querySelector(".js-messages");
+
+if (messages) {
+  const content = messages.querySelector(".js-messages--content");
+
+  scrollToBottom(content);
+
+  // `chat.js` calls this whenever new message received
+  // over ActionCable
+  setCallback(message => {
+    // i.e. after last child
+    content.insertAdjacentHTML("beforeend", message);
+
+    scrollToBottom(content);
+  });
+}
